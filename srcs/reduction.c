2242e8c15a8bd322c1e87e70c636ace05dafd1b9
kratein <dylan.bonin@free.fr>
2016-04-28 14:13:20 +0200

    résumé

--- srcs/reduction.c
+++ srcs/reduction.c
@@ -2,19 +2,38 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/wait.h>
+#define SIZE_BUF 4
 
 int main(int argc, char** argv)
 {
+	int i,pid,n;
+	int tabPid[argc-2]; 
+	char buf[SIZE_BUF];
 	if(argc<3)
 	{
 		perror("Pas assez d'arguments");
-		return 1;
+		exit(EXIT_FAILURE);
 	}
-	int tabPid[argc];
-	int i;
-	for (i = 0; i < argc-3; ++i)
-		if ((tabPid[i] = fork()) <= 0)
-			break;
-printf("toto \n");
+	for(i=0; i<argc-2; i++){
+		pid=fork();
+		while(pid==-1)
+			pid=fork();
+		tabPid[i]=pid;
+		if(tabPid[i]==0){
+			//ouvrir le fichier et lire la 1er ligne
+			//créer autant de thread que la valeur de la  1er ligne du fichier
+			//on regarde la fonction voulu 
+			//chaque thread l'exécute et remonte le résultat à leur processus
+			//le processus effectue la fonction 
+			//il remonte au processus maitre les résultats
+			// le processus maitre fait la fonction
+			//écrit le résultat dans un fichier text
+			printf("pid: %d   ppid: %d\n", getpid(), getppid() );
+		return 0;
+		}
+	}
+	sleep(1);
+	}
 	exit(0);
 }
